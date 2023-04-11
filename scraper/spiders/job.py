c0115f83d96ed4500093e071c90e819a082e6ae5
Povarenskiy <Povarenskiy.rb@yandex.ru>
2023-04-11 21:53:49 +0300

    make spider update

--- scraper/spiders/job.py
+++ scraper/spiders/job.py
@@ -10,7 +10,6 @@ class JobSpider(scrapy.Spider):
     name = 'job'
     allowed_domains = ['hh.ru']
 
-
     def __init__(self,
                 name=None,
                 key_remotely = ['удаленная работа', 'удален'],   
@@ -28,12 +27,17 @@ class JobSpider(scrapy.Spider):
         self.key_exp = key_exp                          # ключи для опыт работы (на исключение)
 
 
-
     @staticmethod
     def _get_code_from_url(url):
         match = re.search(r"\d+", url)
         if match:
             return match.group(0)
+        
+
+    @staticmethod
+    @sync_to_async
+    def _get_item_from_database(code):
+        return HhItems.objects.filter(code=code).first()
         
 
     def start_requests(self):
@@ -45,13 +49,21 @@ class JobSpider(scrapy.Spider):
 
     async def parse_page(self, response, **kwargs):
         """Парсинг страницы поиска с вакансиями"""
-        for href in response.css('.serp-item__title::attr("href")').extract():
-            # проверка на существование вакансии в базе
+        for item in response.css('span[data-page-analytics-event*=vacancy_search_suitable_item]'):
+            
+            href = item.css('a.serp-item__title::attr("href")').extract_first()
             code = self._get_code_from_url(href)
-            try:
-                await sync_to_async(HhItems.objects.get)(code=code)
-            except HhItems.DoesNotExist:
-                yield scrapy.Request(href, callback=self.parse)
+            
+            # проверка на существование вакансии в базе
+            item_from_database= await self._get_item_from_database(code)
+            if not item_from_database:
+                
+                title = item.css('a.serp-item__title::text').extract_first()
+                # если название совпадает по ключевым словам 
+                if any(word_ in title.lower() for word_ in self.key_words) and \
+                    all(word_ not in title.lower() for word_ in self.key_words_exception):            
+
+                    yield scrapy.Request(href, callback=self.parse, meta={'code': code, 'title': title})
         
         # переход на следующую страницу
         next_page = response.css('a[data-qa*=pager-next]::attr("href")').extract_first()
@@ -60,41 +72,39 @@ class JobSpider(scrapy.Spider):
 
 
     def parse(self, response, **kwargs):
-        title = response.css('div.vacancy-title [data-qa*=vacancy-title]::text').extract_first()
 
-        # если название совпадает по ключому словам 
-        if any(word_ in title.lower() for word_ in self.key_words) and \
-            all(word_ not in title.lower() for word_ in self.key_words_exception):            
-            exp = response.css('p.vacancy-description-list-item *::text').extract()
-            exp = ' '.join(exp)
-            
-            # если нет исключений по опыту работы
-            if all(exp_ not in exp.lower() for exp_ in self.key_exp):    
-                location = response.css('p[data-qa*=vacancy-view-location]::text').extract_first() or \
-                    response.css('[data-qa*=vacancy-view-raw-address]::text').extract_first()
-                
-                salary = response.css('div.vacancy-title [data-qa*=vacancy-salary]::text').extract()
-                salary = ' '.join(salary)
+        title = response.meta.get('title')
+        code = response.meta.get('code')
 
-                # если удаленно или совпадает по локации
-                if any(remote_ in exp.lower() for remote_ in self.key_remotely) or \
-                    any(location_ in location.lower() for location_ in self.key_locations):
-                    
-                    url = response.request.url
-                    code = self._get_code_from_url(url)
-
-                    item = ScraperHhItem(
-                        title=title,
-                        salary=salary,
-                        exp=exp,
-                        location=location,
-                        url=url,
-                        code=code,
-                    )
-
-                    yield item
-                        
+        exp = response.css('p.vacancy-description-list-item *::text').extract()
+        exp = ' '.join(exp)
         
+        # если нет исключений по опыту работы
+        if all(exp_ not in exp.lower() for exp_ in self.key_exp):    
+            location = response.css('p[data-qa*=vacancy-view-location]::text').extract_first() or \
+                response.css('[data-qa*=vacancy-view-raw-address]::text').extract_first()
+            
+            salary = response.css('div.vacancy-title [data-qa*=vacancy-salary]::text').extract()
+            salary = ' '.join(salary)
+
+            # если удаленно или совпадает по локации
+            if any(remote_ in exp.lower() for remote_ in self.key_remotely) or \
+                any(location_ in location.lower() for location_ in self.key_locations):
+                
+                url = response.request.url
+    
+                item = ScraperHhItem(
+                    title=title,
+                    salary=salary,
+                    exp=exp,
+                    location=location,
+                    url=url,
+                    code=code,
+                )
+
+                yield item
+                    
+    
 
 
     
