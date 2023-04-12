bfdc533ad3f87bc55ebf5410162566167662ccd1
Povarenskiy <Povarenskiy.rb@yandex.ru>
2023-04-12 07:38:58 +0300

    add str to model

--- hh/models.py
+++ hh/models.py
@@ -10,4 +10,8 @@ class HhItems(models.Model):
     url = models.CharField('Ссылка на вакансию', max_length=250, null=True) 
     location = models.CharField('Место работы', max_length=100, null=True) 
     last_update = models.DateTimeField('Время последнего обновления', auto_created=True, null=True)
+
+
+    def __str__(self) -> str:
+        return str(self.code)
                         
